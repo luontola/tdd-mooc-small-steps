0c2a090662f6cd1e86d3b87b6f4670a209f8470b
Esko Luontola <esko@luontola.fi>
2021-08-12 05:48:45 +0300

    Install @js-temporal/polyfill

--- src/prices.mjs
+++ src/prices.mjs
@@ -1,4 +1,27 @@
 import express from "express";
+import { Temporal, toTemporalInstant } from "@js-temporal/polyfill";
+
+Date.prototype.toTemporalInstant = toTemporalInstant;
+
+// Some examples of using the Temporal API
+// See https://tc39.es/proposal-temporal/docs/
+console.assert(
+  Temporal.PlainDate.from("2000-12-31").equals(
+    new Temporal.PlainDate(2000, 12, 31)
+  )
+);
+console.assert(
+  new Date(42)
+    .toTemporalInstant()
+    .equals(new Temporal.Instant.fromEpochMilliseconds(42))
+);
+console.assert(
+  new Date(42)
+    .toTemporalInstant()
+    .toZonedDateTimeISO("UTC")
+    .toPlainDate()
+    .equals(new Temporal.PlainDate(1970, 1, 1))
+);
 
 function createApp(database) {
   function calculateCost(age, type, date, baseCost) {
