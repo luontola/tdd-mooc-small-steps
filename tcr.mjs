8a68285b33baed921778007817425bbf2bf8552e
Esko Luontola <esko@luontola.fi>
2022-01-28 00:39:17 +0200

    Run tests once before starting TCR

--- tcr.mjs
+++ tcr.mjs
@@ -1,21 +1,40 @@
 import { execSync } from "child_process";
 import fs from "fs";
 
+function test() {
+  execSync("npm run test", {
+    stdio: "inherit",
+    env: { ...process.env, MAX_CHANGES: process.env.MAX_CHANGES || "1" },
+  });
+}
+
+function commit() {
+  console.log("Tests passed -> Commit changes");
+  execSync("git commit --all --message='tcr: tests pass'", {
+    stdio: "inherit",
+  });
+}
+
+function revert() {
+  console.log("Test failed -> Revert changes");
+  execSync("git reset --hard", { stdio: "inherit" });
+}
+
+try {
+  test();
+} catch (e) {
+  console.log("TCR cancelled. Tests need to start green.");
+  process.exit(1);
+}
+
 fs.watch("src", { recursive: true }, (_event, _filename) => {
   const changes = execSync("git diff --numstat", { encoding: "utf8" });
   if (changes !== "") {
     try {
-      execSync("npm run test", {
-        stdio: "inherit",
-        env: { ...process.env, MAX_CHANGES: process.env.MAX_CHANGES || "1" },
-      });
-      console.log("Tests passed -> Commit changes");
-      execSync("git commit --all --message='tcr: tests pass'", {
-        stdio: "inherit",
-      });
+      test();
+      commit();
     } catch (e) {
-      console.log("Test failed -> Revert changes");
-      execSync("git reset --hard", { stdio: "inherit" });
+      revert();
     }
   }
 });
