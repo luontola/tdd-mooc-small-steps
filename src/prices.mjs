17a5c20b6386695ab288f5b5a31891d6d9abbf01
mintsai7777 <king840110@gmail.com>
2023-05-09 22:01:31 +0800

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -77,7 +77,7 @@ function createApp(database) {
   }
 
   function isMonday(date, date2) {
-    return date.getDay() === 1;
+    return date.getDay() === 1 && date2.dayOfWeek === 1;
   }
 
   function isHoliday(date) {
