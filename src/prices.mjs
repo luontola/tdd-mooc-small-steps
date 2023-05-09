80c8809c3ed7b8253ad7ae398c139bbeefc861dd
mintsai7777 <king840110@gmail.com>
2023-05-09 22:21:34 +0800

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -86,6 +86,7 @@ function createApp(database) {
       let holiday = new Date(row.holiday);
       if (
         date2 &&
+        date2.equals(Temporal.PlainDate.from(row.holiday)) &&
         date &&
         date.getFullYear() === holiday.getFullYear() &&
         date.getMonth() === holiday.getMonth() &&
