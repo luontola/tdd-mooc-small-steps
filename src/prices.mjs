60f72737f4457f51f6f80f6340187faacf13c588
mintsai7777 <king840110@gmail.com>
2023-05-09 21:52:26 +0800

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -68,7 +68,7 @@ function createApp(database) {
     return Math.ceil(baseCost * (1 - reduction / 100));
   }
 
-  function calculateReduction(date) {
+  function calculateReduction(date, date2) {
     let reduction = 0;
     if (date && isMonday(date) && !isHoliday(date)) {
       reduction = 35;
