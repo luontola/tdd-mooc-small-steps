fdfbac8f4c2d0696c3c6031628ee322c46e847cd
levoskaa <47329402+levoskaa@users.noreply.github.com>
2024-02-15 19:24:30 +0100

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -83,7 +83,7 @@ function createApp(database) {
   }
 
   function isMonday(date) {
-    return date.getDay() === 1 && convert(date).dayOfWeek === 1;
+    return convert(date).dayOfWeek === 1;
   }
 
   function isHoliday(date) {
