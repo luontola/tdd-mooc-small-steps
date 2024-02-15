4b8c91ee80288ecdffb32c6bfdb33cc9c51dfcef
levoskaa <47329402+levoskaa@users.noreply.github.com>
2024-02-15 19:33:22 +0100

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -43,7 +43,7 @@ function createApp(database) {
     if (type === "night") {
       return calculateCostForNightTicket(age, baseCost);
     } else {
-      return calculateCostForDayTicket(age, date, baseCost);
+      return calculateCostForDayTicket(age, convert(date), baseCost);
     }
   }
 
@@ -61,7 +61,7 @@ function createApp(database) {
   }
 
   function calculateCostForDayTicket(age, date, baseCost) {
-    let reduction = calculateReduction(convert(date));
+    let reduction = calculateReduction(date);
     if (age === undefined) {
       return Math.ceil(baseCost * (1 - reduction / 100));
     }
