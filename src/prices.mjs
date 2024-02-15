abf97decfdf178a99bba297ec0a5e43ff2ac799a
levoskaa <47329402+levoskaa@users.noreply.github.com>
2024-02-15 19:46:30 +0100

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -35,7 +35,7 @@ function createApp(database) {
 
   function parseDate(dateString) {
     if (dateString) {
-      return convert(new Date(dateString));
+      return Temporal.PlainDate.from(dateString);
     }
   }
 
