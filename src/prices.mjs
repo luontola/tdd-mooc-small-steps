73f66e8d88c2bc6495b23a7d75abf6db8083f2ca
levoskaa <47329402+levoskaa@users.noreply.github.com>
2024-02-15 19:19:11 +0100

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -90,6 +90,7 @@ function createApp(database) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
       let holiday = new Date(row.holiday);
+      let holiday2 = convert(holiday);
       if (
         date &&
         date.getFullYear() === holiday.getFullYear() &&
