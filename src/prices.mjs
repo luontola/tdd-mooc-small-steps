ce7a49d6f2a935d7404a8f64c05b2a3d815e9ef0
levoskaa <47329402+levoskaa@users.noreply.github.com>
2024-02-15 19:20:00 +0100

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -95,6 +95,7 @@ function createApp(database) {
       if (
         date &&
         date2 &&
+        date2.equals(holiday2) &&
         date.getFullYear() === holiday.getFullYear() &&
         date.getMonth() === holiday.getMonth() &&
         date.getDate() === holiday.getDate()
